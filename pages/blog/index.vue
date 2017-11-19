85178943139fb848962413f60dfcc51faa31a32d
danieljmurphy91 <danieljmurphy91@gmail.com>
2017-11-19 10:19:53 -0500

    Why We Build Responsive CTA Builder blog post

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -12,14 +12,14 @@
       <div class="container">
         <section class="hero">
           <div class="hero-body has-text-centered">
-            <h1 class="title is-spaced is-2 dodgerText">Tips & Tricks</h1>
+            <h1 class="title is-spaced is-2 dodgerText">The Build Better CTAs Blog</h1>
           </div>
         </section>
         <div class="columns">
           <div class="column is-half">
             <div class="box helpBox">
-              <h1 class="title is-5 is-spaced">Introduction to Responsive CTA Builder</h1>
-              <p class="subtitle is-6">Introduction Meta</p>
+              <h1 class="title is-5 is-spaced">Why We Build Responsive CTA Builder</h1>
+              <p class="subtitle is-6">About a year ago, we made a discovery. The calls-to-action (CTAs) on our company’s blog weren’t performing as well as they could. So we decided to fix that.</p>
               <router-link to="/blog/introduction">Read</router-link>
             </div>
           </div>
